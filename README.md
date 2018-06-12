1b7df5ba379799619363ddea9a0877d846326af6
afeiship <1290657123@qq.com>
2018-06-12 18:02:25 +0800

    issue: intail

---
+++ README.md
@@ -0,0 +1,12 @@
+# next-auction-price
+> Auction price for next
+
+## install:
+```bash
+npm install -S afeiship/next-auction-price --registry=https://registry.npm.taobao.org
+```
+
+## usage:
+```js
+//DOCS here!
+```
---
+++ src/next-auction-price.js
@@ -0,0 +1,26 @@
+(function () {
+
+  var global = global || this || self || window;
+  var nx = global.nx || require('next-js-core2');
+
+  nx.auctionPrice = function (inData) {
+    var status = inData.status;
+    var leave = inData.leave;
+    switch (true) {
+      case status === 'N':
+        return { key: 'beginPrice', value: '起拍价' };
+      case status === 'A':
+        return { key: 'currentPrice', value: '当前价' };
+      case status === 'F' && leave:
+        return { key: 'hammerPrice', value: '成交价' };
+      case status === 'F' && !leave:
+        return { key: null, value: '流拍' };
+    }
+  };
+
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.auctionPrice;
+  }
+
+}());

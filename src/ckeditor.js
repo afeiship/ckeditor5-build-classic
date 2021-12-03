01c497400571d68c23417c578f3d0a2b69234c4f
afeiship <1290657123@qq.com>
2021-12-03 09:31:01 +0800

    chore: add prettier

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -2,53 +2,53 @@
  * @license Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
-import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor.js";
-import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat.js";
-import AutoLink from "@ckeditor/ckeditor5-link/src/autolink.js";
-import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote.js";
-import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold.js";
-import Code from "@ckeditor/ckeditor5-basic-styles/src/code.js";
-import CodeBlock from "@ckeditor/ckeditor5-code-block/src/codeblock.js";
-import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials.js";
-import FontBackgroundColor from "@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js";
-import FontColor from "@ckeditor/ckeditor5-font/src/fontcolor.js";
-import FontSize from "@ckeditor/ckeditor5-font/src/fontsize.js";
-import GeneralHtmlSupport from "@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js";
-import Heading from "@ckeditor/ckeditor5-heading/src/heading.js";
-import HorizontalLine from "@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js";
-import HtmlComment from "@ckeditor/ckeditor5-html-support/src/htmlcomment.js";
-import HtmlEmbed from "@ckeditor/ckeditor5-html-embed/src/htmlembed.js";
-import Image from "@ckeditor/ckeditor5-image/src/image.js";
-import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption.js";
-import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize.js";
-import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle.js";
-import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar.js";
-import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload.js";
-import Indent from "@ckeditor/ckeditor5-indent/src/indent.js";
-import IndentBlock from "@ckeditor/ckeditor5-indent/src/indentblock.js";
-import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js";
-import Link from "@ckeditor/ckeditor5-link/src/link.js";
-import LinkImage from "@ckeditor/ckeditor5-link/src/linkimage.js";
-import List from "@ckeditor/ckeditor5-list/src/list.js";
-import ListStyle from "@ckeditor/ckeditor5-list/src/liststyle.js";
-import PageBreak from "@ckeditor/ckeditor5-page-break/src/pagebreak.js";
-import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js";
-import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js";
-import RemoveFormat from "@ckeditor/ckeditor5-remove-format/src/removeformat.js";
-import SourceEditing from "@ckeditor/ckeditor5-source-editing/src/sourceediting.js";
-import StandardEditingMode from "@ckeditor/ckeditor5-restricted-editing/src/standardeditingmode.js";
-import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough.js";
-import Subscript from "@ckeditor/ckeditor5-basic-styles/src/subscript.js";
-import Superscript from "@ckeditor/ckeditor5-basic-styles/src/superscript.js";
-import Table from "@ckeditor/ckeditor5-table/src/table.js";
-import TableCaption from "@ckeditor/ckeditor5-table/src/tablecaption.js";
-import TableCellProperties from "@ckeditor/ckeditor5-table/src/tablecellproperties";
-import TableProperties from "@ckeditor/ckeditor5-table/src/tableproperties";
-import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar.js";
-import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation.js";
-import TodoList from "@ckeditor/ckeditor5-list/src/todolist";
-import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline.js";
-import WordCount from "@ckeditor/ckeditor5-word-count/src/wordcount.js";
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import AutoLink from '@ckeditor/ckeditor5-link/src/autolink.js';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code.js';
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock.js';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
+import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js';
+import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor.js';
+import FontSize from '@ckeditor/ckeditor5-font/src/fontsize.js';
+import GeneralHtmlSupport from '@ckeditor/ckeditor5-html-support/src/generalhtmlsupport.js';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading.js';
+import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js';
+import HtmlComment from '@ckeditor/ckeditor5-html-support/src/htmlcomment.js';
+import HtmlEmbed from '@ckeditor/ckeditor5-html-embed/src/htmlembed.js';
+import Image from '@ckeditor/ckeditor5-image/src/image.js';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption.js';
+import ImageResize from '@ckeditor/ckeditor5-image/src/imageresize.js';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle.js';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar.js';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload.js';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent.js';
+import IndentBlock from '@ckeditor/ckeditor5-indent/src/indentblock.js';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic.js';
+import Link from '@ckeditor/ckeditor5-link/src/link.js';
+import LinkImage from '@ckeditor/ckeditor5-link/src/linkimage.js';
+import List from '@ckeditor/ckeditor5-list/src/list.js';
+import ListStyle from '@ckeditor/ckeditor5-list/src/liststyle.js';
+import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak.js';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
+import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
+import SourceEditing from '@ckeditor/ckeditor5-source-editing/src/sourceediting.js';
+import StandardEditingMode from '@ckeditor/ckeditor5-restricted-editing/src/standardeditingmode.js';
+import Strikethrough from '@ckeditor/ckeditor5-basic-styles/src/strikethrough.js';
+import Subscript from '@ckeditor/ckeditor5-basic-styles/src/subscript.js';
+import Superscript from '@ckeditor/ckeditor5-basic-styles/src/superscript.js';
+import Table from '@ckeditor/ckeditor5-table/src/table.js';
+import TableCaption from '@ckeditor/ckeditor5-table/src/tablecaption.js';
+import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
+import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
+import TodoList from '@ckeditor/ckeditor5-list/src/todolist';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
+import WordCount from '@ckeditor/ckeditor5-word-count/src/wordcount.js';
 
 class Editor extends ClassicEditor {}
 
@@ -108,121 +108,121 @@ Editor.defaultConfig = {
     shouldGroupWhenFull: true,
     isFloating: true,
     items: [
-      "undo",
-      "redo",
-      "heading",
-      "|",
-      "bold",
-      "italic",
-      "fontColor",
-      "fontBackgroundColor",
-      "fontSize",
-      "strikethrough",
-      "underline",
-      "superscript",
-      "subscript",
-      "removeFormat",
-      "|",
-      "link",
-      "bulletedList",
-      "numberedList",
-      "blockQuote",
-      "|",
-      "outdent",
-      "indent",
-      "|",
-      "code",
-      "codeBlock",
-      "htmlEmbed",
-      "sourceEditing",
-      "todoList",
-      "|",
-      "imageUpload",
-      "insertTable",
+      'undo',
+      'redo',
+      'heading',
+      '|',
+      'bold',
+      'italic',
+      'fontColor',
+      'fontBackgroundColor',
+      'fontSize',
+      'strikethrough',
+      'underline',
+      'superscript',
+      'subscript',
+      'removeFormat',
+      '|',
+      'link',
+      'bulletedList',
+      'numberedList',
+      'blockQuote',
+      '|',
+      'outdent',
+      'indent',
+      '|',
+      'code',
+      'codeBlock',
+      'htmlEmbed',
+      'sourceEditing',
+      'todoList',
+      '|',
+      'imageUpload',
+      'insertTable',
     ],
   },
-  language: "zh-cn",
+  language: 'zh-cn',
   image: {
     toolbar: [
-      "imageTextAlternative",
-      "toggleImageCaption",
-      "imageStyle:inline",
-      "imageStyle:block",
-      "imageStyle:side",
-      "linkImage",
+      'imageTextAlternative',
+      'toggleImageCaption',
+      'imageStyle:inline',
+      'imageStyle:block',
+      'imageStyle:side',
+      'linkImage',
     ],
   },
   heading: {
     options: [
-      { model: "paragraph", title: "Paragraph", class: "ck-heading_paragraph" },
+      { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
       {
-        model: "heading1",
-        view: "h1",
-        title: "Heading 1",
-        class: "ck-heading_heading1",
+        model: 'heading1',
+        view: 'h1',
+        title: 'Heading 1',
+        class: 'ck-heading_heading1',
       },
       {
-        model: "heading2",
-        view: "h2",
-        title: "Heading 2",
-        class: "ck-heading_heading2",
+        model: 'heading2',
+        view: 'h2',
+        title: 'Heading 2',
+        class: 'ck-heading_heading2',
       },
       {
-        model: "heading3",
-        view: "h3",
-        title: "Heading 3",
-        class: "ck-heading_heading3",
+        model: 'heading3',
+        view: 'h3',
+        title: 'Heading 3',
+        class: 'ck-heading_heading3',
       },
       {
-        model: "heading4",
-        view: "h4",
-        title: "Heading 4",
-        class: "ck-heading_heading4",
+        model: 'heading4',
+        view: 'h4',
+        title: 'Heading 4',
+        class: 'ck-heading_heading4',
       },
       {
-        model: "heading5",
-        view: "h5",
-        title: "Heading 5",
-        class: "ck-heading_heading5",
+        model: 'heading5',
+        view: 'h5',
+        title: 'Heading 5',
+        class: 'ck-heading_heading5',
       },
       {
-        model: "heading6",
-        view: "h6",
-        title: "Heading 6",
-        class: "ck-heading_heading6",
+        model: 'heading6',
+        view: 'h6',
+        title: 'Heading 6',
+        class: 'ck-heading_heading6',
       },
     ],
   },
   table: {
     contentToolbar: [
-      "tableColumn",
-      "tableRow",
-      "mergeTableCells",
-      "tableCellProperties",
-      "tableProperties",
+      'tableColumn',
+      'tableRow',
+      'mergeTableCells',
+      'tableCellProperties',
+      'tableProperties',
     ],
   },
   codeBlock: {
     languages: [
-      { language: "plaintext", label: "Plain text" }, // The default language.
-      { language: "shell", label: "Shell" },
-      { language: "html", label: "HTML" },
-      { language: "javascript", label: "JavaScript" },
-      { language: "json", label: "JSON" },
-      { language: "typescript", label: "TypeScript" },
-      { language: "css", label: "CSS" },
-      { language: "java", label: "Java" },
-      { language: "kotlin", label: "Kotlin" },
-      { language: "php", label: "PHP" },
-      { language: "python", label: "Python" },
-      { language: "ruby", label: "Ruby" },
-      { language: "rust", label: "Rust" },
-      { language: "xml", label: "XML" },
-      { language: "apache", label: "Config" },
-      { language: "ini", label: "Ini" },
-      { language: "yml", label: "Yaml" },
-      { language: "sql", label: "SQL" },
-      { language: "c", label: "C" },
+      { language: 'plaintext', label: 'Plain text' }, // The default language.
+      { language: 'shell', label: 'Shell' },
+      { language: 'html', label: 'HTML' },
+      { language: 'javascript', label: 'JavaScript' },
+      { language: 'json', label: 'JSON' },
+      { language: 'typescript', label: 'TypeScript' },
+      { language: 'css', label: 'CSS' },
+      { language: 'java', label: 'Java' },
+      { language: 'kotlin', label: 'Kotlin' },
+      { language: 'php', label: 'PHP' },
+      { language: 'python', label: 'Python' },
+      { language: 'ruby', label: 'Ruby' },
+      { language: 'rust', label: 'Rust' },
+      { language: 'xml', label: 'XML' },
+      { language: 'apache', label: 'Config' },
+      { language: 'ini', label: 'Ini' },
+      { language: 'yml', label: 'Yaml' },
+      { language: 'sql', label: 'SQL' },
+      { language: 'c', label: 'C' },
     ],
   },
 };
